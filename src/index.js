311f80d2033a20174631d4e6f071c556c739a2ae
Sungjun <hongsi140626@gmail.com>
2019-06-09 21:05:06 -0400

    desgin3

--- src/index.js
+++ src/index.js
@@ -4,9 +4,12 @@ import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { createBrowserHistory } from "history";
+
+const history = createBrowserHistory();
 
 ReactDOM.render(
-    <BrowserRouter basename="/hong_portfolio">
+    <BrowserRouter history={history}>
         <App />
     </BrowserRouter>, document.getElementById('root'));
  
