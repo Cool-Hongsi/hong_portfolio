5208b66555f91b171b4ff0ada38e0cd5519476ce
Sungjun <hongsi140626@gmail.com>
2019-06-09 21:15:25 -0400

    route

--- src/App.js
+++ src/App.js
@@ -9,20 +9,27 @@ import Project from './components/js/Project';
 export default class App extends React.Component{
   render(){
     return(
-      <Switch>
-        <Route exact path="/" render={() => (
-          <Landing />
-        )}/>
-        <Route path="/about" render={() => (
-          <About />
-        )}/>
-        <Route path="/skill" render={() => (
-          <Skill />
-        )}/>
-        <Route path="/project" render={() => (
-          <Project />
-        )}/>
-      </Switch>
+      // <Switch>
+      //   <Route exact path="/" render={() => (
+      //     <Landing />
+      //   )}/>
+      //   <Route path="/about" render={() => (
+      //     <About />
+      //   )}/>
+      //   <Route path="/skill" render={() => (
+      //     <Skill />
+      //   )}/>
+      //   <Route path="/project" render={() => (
+      //     <Project />
+      //   )}/>
+      // </Switch>
+      <div>
+      <Route exact path="/" component={Landing} />
+      <Route exact path="/about" component={About} />
+      <Route exact path="/skill" component={Skill} />
+      <Route exact path="/project" component={Project} />
+      </div>
+
     )
   }
 }
