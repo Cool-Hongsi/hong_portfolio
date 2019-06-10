fa429e4edce95b4700c1ba2b4e42376876d5d4c0
Sungjun <hongsi140626@gmail.com>
2019-06-10 15:47:08 -0400

    router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Landing from './components/js/Landing';
 import About from './components/js/About';
 import Skill from './components/js/Skill';
@@ -9,12 +9,14 @@ import Project from './components/js/Project';
 export default class App extends React.Component{
   render(){
     return(
-      <div>
-        <Route exact path="/" component={Landing} />
-        <Route exact path="/about" component={About} />
-        <Route exact path="/skill" component={Skill} />
-        <Route exact path="/project" component={Project} />
-      </div>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Landing} />
+          <Route path="/about" component={About} />
+          <Route path="/skill" component={Skill} />
+          <Route path="/project" component={Project} />
+        </Switch>
+      </Router>
     )
   }
 }
--- src/components/js/NavBar.js
+++ src/components/js/NavBar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import '../css/NavBar.css';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 
 export default class NavBar extends React.Component{
     render(){
@@ -14,19 +14,20 @@ export default class NavBar extends React.Component{
                         <i></i> 
                     </label>
                     <div className="logo">
+                        {/* <Link to="/" style={{textDecoration: "none", color: "#000"}}>HONG</Link> */}
                         <a href="/" style={{textDecoration: "none", color: "#000"}}>HONG</a>
                     </div>
                     <div className="nav-wrapper">
                         <ul>
-                            <li><Link to="/" style={{textDecoration: "none", color: "#000"}}>Home</Link></li>
+                            {/* <li><Link to="/" style={{textDecoration: "none", color: "#000"}}>Home</Link></li>
                             <li><Link to="/about" style={{textDecoration: "none", color: "#000"}}>About</Link></li>
                             <li><Link to="/skill" style={{textDecoration: "none", color: "#000"}}>Skill</Link></li>
-                            <li><Link to="/project" style={{textDecoration: "none", color: "#000"}}>Project</Link></li>
+                            <li><Link to="/project" style={{textDecoration: "none", color: "#000"}}>Project</Link></li> */}
 
-                            {/* <li><a href="/" style={{textDecoration: "none", color: "#000"}}>Home</a></li>
+                            <li><a href="/" style={{textDecoration: "none", color: "#000"}}>Home</a></li>
                             <li><a href="/about" style={{textDecoration: "none", color: "#000"}}>About</a></li>
                             <li><a href="/skill" style={{textDecoration: "none", color: "#000"}}>Skill</a></li>
-                            <li><a href="/project" style={{textDecoration: "none", color: "#000"}}>Project</a></li> */}
+                            <li><a href="/project" style={{textDecoration: "none", color: "#000"}}>Project</a></li>
                         </ul>
                     </div>
                 </nav>
